5f59237da14711cc4600a6fd40a0881dffecfb3f
1067563549 <1067563549@qq.com>
2021-11-30 10:56:26 +0800

    commit

---
+++ pro2/Neural Network/neural.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense,Dropout,Activation
+from tensorflow.python.keras.optimizers import SGD
+
+def get_data(filename):
+    x=[]
+    y=[]
+    with open(filename,'r') as fp:
+        lines=fp.readlines()
+    for i in range(len(lines)):
+        line=lines[i].strip().replace(' ','').split(',')
+        x.append([float(line[i]) for i in range(16)])
+        y.append(float(line[16]))
+    return np.array(x),np.array(y)
+
+x_train,y_train=get_data('pendigits.tra')
+x_test,y_test=get_data('pendigits.tes')
+# print(x_train)
+
+sgd=SGD(lr=0.01,decay=1e-6,momentum=0.9,nesterov=True)
+model=Sequential()
+model.add(Dense(units=256,input_dim=16,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=16,activation='softmax'))
+model.compile(loss='sparse_categorical_crossentropy',optimizer=sgd,metrics=['accuracy'])
+
+history=model.fit(x_train,y_train,batch_size=128,epochs=20,verbose=1)
+score=model.evaluate(x_test,y_test,batch_size=128)
+# print()
+# print(score[0])
+# print(1)
